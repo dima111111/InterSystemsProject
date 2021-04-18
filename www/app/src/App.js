84b670ab9898c44af3744c082d1413e3e50d68e5
Ilgiz <38355923+EkT1oN@users.noreply.github.com>
2021-04-18 16:35:43 +0300

    Add function for get Doctors from db

--- www/app/src/App.js
+++ www/app/src/App.js
@@ -10,67 +10,44 @@ function sendJsonToAddr(addr, data) {
 	});
 }
 
-function App() {
-
-	console.log("pep")
-	function loginForm(event) {
-		event.preventDefault()
-		const fio = event.target.FIO.value;
-		var birthDate = event.target.birthDate.value
-		console.log(birthDate)
-		// birthDate = birthDate.replace(/[\-]/g,'');
-		// console.log(birthDate)
-		const email = event.target.email.value
-		const phone = event.target.phone.value
-		const dict = {
-			FIO: fio,
-			birthDate: birthDate,
-			email: email,
-			phone: phone
-		}
-		sendJsonToAddr('http://localhost:9098/bmstuapi/login', dict)
+function loginForm(event) {
+	event.preventDefault()
+	const fio = event.target.FIO.value;
+	const birthDate = event.target.birthDate.value
+	const email = event.target.email.value
+	const phone = event.target.phone.value
+	const dict = {
+		FIO: fio,
+		birthDate: birthDate,
+		email: email,
+		phone: phone
 	}
+	sendJsonToAddr('http://localhost:9098/bmstuapi/login', dict)
+}
+
+function getDoctors() {
+	console.log("get Doctors")
+	var addr = 'http://localhost:9098/bmstuapi/getDoctors'
+	const json = {}
+	axios({
+		method: 'get',
+		url: addr,
+		data: json
+	})
+	console.log(json)
+}
+
+function App() {
 	return (
-		<form onSubmit={loginForm}>
-			<div>
-				<p>
-				<label><b>FIO</b>
-				<input type="text" placeholder="Enter FIO" name="FIO" required />
-				</label>
-				</p>
-
-				<p>
-				<label><b>Birth Date</b>
-				<input type="date" placeholder="Enter date" name="birthDate" required />
-				</label>
-				</p>
-
-				<p>
-				<label><b>Email</b>
-				<input type="email" placeholder="Enter email" name="email" required />
-				</label>
-				</p>
-
-				<p>
-				<label><b>Phone</b>
-				<input type="text" placeholder="Enter phone" name="phone" required />
-				</label>
-				</p>
-				
-				<p>
-				<button type="submit">Login</button>
-				</p>
-			</div>
-		</form>
-
-		// <form action="http://localhost:9098/bmstuapi/login" method="POST">
-		// 	<div class="container">
-		// 		<label for="FIO"><b>FIO</b></label>
-		// 		<input type="text" placeholder="Enter FIO" name="FIO" required>
-		// 		<button type="submit">Login</button>
-		// 	</div>
-		// </form>
-	);
+		<p>
+			Welcome to Dentistry
+			<p>
+			<a href='loginForm.html'>
+				Register
+			</a>
+			</p>
+		</p>
+	)
 }
 
 export default App;
